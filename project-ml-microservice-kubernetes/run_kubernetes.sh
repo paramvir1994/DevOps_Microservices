b190ebdaf65d1f5eeb6f5fcf35f957052087820c
Paramvir Singh <paramvir.singh@clio.com>
2020-06-22 18:38:15 -0700

    Adding commands to run kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=paramvir1994/app:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app\
+    --generator=run-pod/v1\
+    --image=${dockerpath}\
+    --port=80 --labels app=app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 8000:80
